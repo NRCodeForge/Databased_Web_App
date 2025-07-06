e4392befa9d9232649bb907e35190f2deb1138ea
VeryAngryFrog <ajost@stud.hs-bremen.de>
2025-07-06 20:47:15 +0200

    Alex
    Startseite Overhaul

--- src/app/startseite/startseite.component.ts
+++ src/app/startseite/startseite.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { SectionFormatComponent } from '../section-format/section-format.component';
 import { CommonModule } from '@angular/common';
-import {RouterModule} from "@angular/router";
-import {ReactiveFormsModule} from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-startseite',
   standalone: true,
-  imports: [CommonModule, SectionFormatComponent,ReactiveFormsModule ,RouterModule ],
+  imports: [CommonModule, SectionFormatComponent, ReactiveFormsModule, RouterModule ],
   templateUrl: './startseite.component.html',
   styleUrls: ['./startseite.component.css']
 })
-export class StartseiteComponent {
-
-}
+export class StartseiteComponent {}
