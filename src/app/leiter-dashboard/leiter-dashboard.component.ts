16c457525b875f217a6c21f8a4905c3e69e07d67
TBY01Z <rieckersniclas@gmail.com>
2025-07-08 12:13:07 +0200

    update leiter dashboard

--- src/app/leiter-dashboard/leiter-dashboard.component.ts
+++ src/app/leiter-dashboard/leiter-dashboard.component.ts
@@ -1,5 +1,3 @@
-import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -7,38 +5,56 @@ import { ContentService } from '../services/content.service';
 import { CategoryService } from '../services/category.service';
 import { Post } from '../models/post';
 import { Category } from '../models/category';
-
+import { AuthService } from '../services/auth.service';
+import { SectionFormatComponent } from '../section-format/section-format.component';
 
 @Component({
   selector: 'app-leiter-dashboard',
   standalone: true,
-  imports: [CommonModule, FormsModule, HttpClientModule],
+  imports: [CommonModule, ReactiveFormsModule, SectionFormatComponent],
   templateUrl: './leiter-dashboard.component.html',
   styleUrls: ['./leiter-dashboard.component.css']
 })
-export class LeiterDashboardComponent {
-  bildDatei: File | null = null;
+export class LeiterDashboardComponent implements OnInit {
+  // ... restlicher Code bleibt unverändert
+  @Input() post: Post | null = null;
+  @Output() postSaved = new EventEmitter<Post>();
+  postForm: FormGroup;
   categories: Category[] = [];
-  beitraegeFrom: FormGroup;
-  titel = '';
-  inhalt = '';
+  selectedFormat: string = 'textBild';
+  selectedImageURL: string = '';
 
   constructor(
-    private http: HttpClient,
     private fb: FormBuilder,
     private contentService: ContentService,
-    private categoryService: CategoryService
+    private categoryService: CategoryService,
+    private authService: AuthService,
+
   ) {
-    this.beitraegeFrom = this.fb.group({
+    this.postForm = this.fb.group({
       Titel: ['', Validators.required],
       Inhalt: ['', Validators.required],
       KategorieID: ['', Validators.required],
-      Bild: null
+      Bild: ['']
     });
   }
 
-  onFileSelected(event: any) {
-    this.bildDatei = event.target.files[0];
+  ngOnInit(): void {
+    this.loadCategories();
+    if (this.post) {
+      this.postForm.patchValue(this.post);
+    }
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const reader = new FileReader();
+      reader.onload = e => {
+        this.selectedImageURL = reader.result as string;
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
   }
 
   loadCategories(): void {
@@ -47,33 +63,38 @@ export class LeiterDashboardComponent {
     });
   }
 
-  abteilungAuswahl: string = '';
-  onAbteilungChange(event: Event) {
-    const selectElement = event.target as HTMLSelectElement;
-    this.abteilungAuswahl = selectElement.value;
-    console.log('Gewählte Abteilung:', this.abteilungAuswahl);
-  }
-  
-  speichern() {
-    if (!this.bildDatei) {
-      alert('Bitte ein Bild auswählen.');
-      return;
+  getMode(): 1 | 2 | 3 | 4 {
+    switch (this.selectedFormat) {
+      case 'textBild':
+        return 1;
+      case 'bildText':
+        return 2;
+      case 'nurText':
+        return 3;
+      case 'nurBild':
+        return 4;
+      default:
+        return 1;
     }
-
-    const postData = this.beitraegeFrom.value;
-    this.http.post('/api/beitrag-erstellen', postData).subscribe({
-      next: (res) => {
-        alert('Beitrag erfolgreich gespeichert!');
-        this.titel = '';
-        this.inhalt = '';
-        this.bildDatei = null;
-      },
-      error: (err) => {
-        console.error(err);
-        alert('Fehler beim Speichern des Beitrags.');
-      }
-    });
   }
 
-
+  savePost(): void {
+    if (this.postForm.valid) {
+      const postData = this.postForm.value;
+      postData.UserID = this.authService.getUserID();
+      if (postData.UserID != null) {
+        if (this.post) {
+          this.contentService.updatePost(this.post.BeitragsID, postData).subscribe(savedPost => {
+            this.postSaved.emit(savedPost);
+          });
+        } else {
+          this.contentService.createPost(postData).subscribe(savedPost => {
+            this.postSaved.emit(savedPost);
+          });
+        }
+      } else {
+        console.log("User konnte nicht gefunden werden")
+      }
+    }
+  }
 }
